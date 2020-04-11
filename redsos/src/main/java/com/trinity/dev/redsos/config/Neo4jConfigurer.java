d6ea12441dd478248451d31b019794155b5a6f0b
oiprado <oiprado@gmail.com>
2020-04-11 00:44:48 -0500

    log variables

--- redsos/src/main/java/com/trinity/dev/redsos/config/Neo4jConfigurer.java
+++ redsos/src/main/java/com/trinity/dev/redsos/config/Neo4jConfigurer.java
@@ -5,10 +5,15 @@
  */
 package com.trinity.dev.redsos.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+
+import javax.annotation.PostConstruct;
 
 
 /**
@@ -18,8 +23,23 @@ import org.springframework.core.env.Environment;
 @Configuration
 public class Neo4jConfigurer {
 
+    @Value("${data.neo4j.username}")
+    private String username;
+    @Value("${data.neo4j.password}")
+    private String password;
+    @Value("${data.neo4j.uri}")
+    private String uri;
     @Autowired
     private Environment environment;
-    
-   
+
+    private final Logger log = LoggerFactory.getLogger(Neo4jConfigurer.class);
+
+   @PostConstruct
+    public void showVaraibles() {
+
+       log.debug(String.format("NEO4J_USER=%s",username));
+       log.debug(String.format("NEO4J_PASSWORD=%s",password));
+       log.debug(String.format("NEO4J_URI=%s",uri));
+
+   }
 }
